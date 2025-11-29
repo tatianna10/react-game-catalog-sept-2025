940b5ad30e463ce4c3ab43c1dc4c7caef38ae5c9
Tatyana Atanasova <106607660+tatianna10@users.noreply.github.com>
2025-11-29 19:52:08 +0200

    Add logout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,58 +1,61 @@
-import { Route, Routes } from "react-router";
-import { useState } from "react";
-import Header from "./components/header/Header";
-import Footer from "./components/footer/Footer";
-import Home from "./components/home/Home";
-import Catalog from "./components/catalog/Catalog";
-import Details from "./components/details/Details";
-import Login from "./components/login/Login";
+import { Route, Routes } from "react-router"
+import { useState } from "react"
+
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Home from "./components/home/Home"
+import Catalog from "./components/catalog/Catalog"
+import Details from "./components/details/Details"
+import Login from "./components/login/Login"
+import Logout from "./components/logout/Logout"
 import Register from "./components/register/register.jsx";
-import GameCreate from "./components/game-create/GameCreate.jsx";
 
 function App() {
-  const [registerdUsers, setRegisteredUsers] = useState([]);
-  const [user, setUser] = useState(null);
+    const [registerdUsers, setRegisteredUsers] = useState([]);
+    const [user, setUser] = useState(null);
 
-  const registerHandler = (email, password) => {
-    if (registerdUsers.some(user => user.email === email)) {
-      throw new Error('Email is taken!');
-    }
+    const registerHandler = (email, password) => {
+        if (registerdUsers.some(user => user.email === email)) {
+            throw new Error('Email is taken!');
+        }
 
-    const newUser = { email, password };
+        const newUser = { email, password };
 
-    setRegisteredUsers(state => [...state, newUser]);
+        setRegisteredUsers(state => [...state, newUser]);
 
-    // Login user after register
-    setUser(newUser);
-  };
+        // Login user after register
+        setUser(newUser);
+    };
 
-  const loginHandler = (email, password) => {
-    const user = registerdUsers.find(u => u.email === email && u.password === password);
-    if (!user) {
-      throw new Error('Invalid email or password');
-    }
+    const loginHandler = (email, password) => {
+        const user = registerdUsers.find(u => u.email === email && u.password === password);
+        if (!user) {
+            throw new Error('Invalid email or password')
+        }
 
-    setUser(user);
-  };
+        setUser(user);
+    };
 
+    const logoutHandler = () => {
+        setUser(null);
+    };
 
+    return (
+        <>
+            <Header user={user} />
 
-  return (
-    <>
-      <Header user={user} />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/games" element={<Catalog />} />
+                <Route path="/games/:gameId/details" element={<Details user={user} />} />
+                <Route path="/register" element={<Register onRegister={registerHandler} />} />
+                <Route path="/login" element={<Login onLogin={loginHandler} />} />
+                <Route path="/logout" element={<Logout onLogout={logoutHandler} />} />
+            </Routes>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games" element={<Catalog />} />
-        <Route path="/games/:gameId/details" element={<Details user={user} />} />
-        <Route path="/games/create" element={<GameCreate />} />
-        <Route path="/register" element={<Register onRegister={registerHandler} />} />
-        <Route path="/login" element={<Login onLogin={loginHandler} />} />
-      </Routes>
-
-      <Footer />
-    </>
-  );
+            <Footer />
+        </>
+    )
 }
 
-export default App;
+export default App
---
+++ client/src/components/logout/Logout.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from "react-router";
+
+export default function Logout({
+    onLogout,
+}) {
+
+    onLogout();
+    return <Navigate to="/" />;
+
+}
